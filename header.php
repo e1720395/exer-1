53d864d80de14687bccf63bf8f858fbf6528985b
Valera <e1720395@cmaisonneuve.qc.ca>
2020-09-28 11:43:11 -0400

    Modification Php

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'exer-1' ); ?></a>
+	<div><p>Php</p></div>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
